9284df8c22feb70c63144111801d110d95944064
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-07 18:08:48 +0100

    add game logic

---
+++ main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG")
+screen.tracer(0)
+
+right_paddle = Paddle((350,0))
+left_paddle = Paddle((-350,0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+
+game_is_on = True
+
+
+while game_is_on:
+
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    #Detech collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #Detech collision with paddle
+    if (ball.distance(right_paddle) < 50 and ball.xcor() > 320) or (ball.distance(left_paddle) < 50 and ball.xcor() > -320):
+        ball.bounce_x()
+
+
+
+
+
+
+
+screen.exitonclick()
